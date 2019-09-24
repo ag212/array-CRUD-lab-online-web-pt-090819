5b646577ebacf2938bbae042bc069ba3880fdbfb
Abraham Galva <ag212@icloud.com>
2019-09-24 21:01:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  new_array = ["winter","autumn","summer","spring"]
+  seasons = ["winter","autumn","summer","spring"]
 end
 
 def add_element_to_end_of_array(array, element)
