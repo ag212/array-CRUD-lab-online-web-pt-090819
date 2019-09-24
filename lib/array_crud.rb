a5c33295d6f607a4cc27f82c728cf91ac01c0330
Abraham Galva <ag212@icloud.com>
2019-09-24 21:02:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  seasons << "equinox"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  seasons.unshift("equinox")
 end
 
 def remove_element_from_end_of_array(array)
